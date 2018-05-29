83b12398b4d714f43e5f81c1886ee142aa2c8847
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2018-05-29 10:56:34 -0300

    Revert "firewalld rules"
    This reverts commit 7d20ff70b83023c4d6e64a743ea49bdbe93c39a4.

--- install_otrs.sh
+++ install_otrs.sh
@@ -64,14 +64,6 @@ sed -i s/enforcing/permissive/g /etc/selinux/config
 
 setenforce 0
 
-
-## Firewalld
-
-firewall-cmd --zone=public --add-service=http
-firewall-cmd --zone=public --add-service=https
-
-
-
 ## Instalar mariadb-server
 
 yum -y install mariadb-server expect epel-release
